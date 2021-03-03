eb9006f008da749e1a17a70944e95fcd9be5f092
jonathanbasuki <basukijonathan4@gmail.com>
2021-03-03 09:12:41 +0700

    source data changed

--- index.php
+++ index.php
@@ -42,9 +42,12 @@
               <p>Last Update: <?= lastUpdate(); ?></p>
             </div>
             <div class="card-body">
-              <h5 class="card-title fs-3 mb-3">Gempabumi M 5.0+ Terakhir</h5>
+
+              <!-- Gempabumi Terkini -->
+
+              <h5 class="card-title fs-3 mb-3">Gempabumi Terkini</h5>
                 <?php
-                  $gempa = "https://data.bmkg.go.id/autogempa.xml";
+                  $gempa = "https://data.bmkg.go.id/DataMKG/TEWS/autogempa.xml";
                   $temp = file_get_contents($gempa);
                   $xml = simplexml_load_string($temp);
 
@@ -80,18 +83,6 @@
                     <div class="bg-info text-dark p-3 highlight">
                       <h4 class="text-uppercase fw-light fs-3">Wilayah</h4>
                       <div class="row p-1">
-                        <div class="col-md-6">
-                          <b>
-                            <p class="mb-0">Terdekat:</p>
-                            <ul>
-                              <li><?= $data->Wilayah1 ?></li>
-                              <li><?= $data->Wilayah2 ?></li>
-                              <li><?= $data->Wilayah3 ?></li>
-                              <li><?= $data->Wilayah4 ?></li>
-                              <li><?= $data->Wilayah5 ?></li>
-                            </ul>
-                          </b>
-                        </div>
                         <div class="col-md-3">
                           <b>
                             <p class="mb-0">Bujur: <?= $data->Bujur; ?></p>
@@ -99,63 +90,13 @@
                             <p>Kedalaman: <?= $data->Kedalaman; ?></p>
                           </b>                         
                         </div>
-                        <div class="col-md-3">
-                          <img src="https://data.bmkg.go.id/eqmap.gif" width="200px" alt="Lokasi Gempa">              
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <?php endforeach; ?>
-
-                <h5 class="card-title fs-3 mb-3">Gempabumi Berpotensi Tsunami Terakhir</h5>
-                <?php
-                  $source = "https://data.bmkg.go.id/lasttsunami.xml";
-                  $temp = file_get_contents($source);
-                  $xml = simplexml_load_string($temp);
-
-                  foreach($xml as $data) :
-                ?>
-
-                <div class="row d-flex align-items-center">
-                  <div class="col-md-4 mb-2">
-                    <div class="bg-success text-light p-3 highlight">
-                      <h4 class="text-uppercase fw-light fs-3">Waktu</h4>
-                      <b>
-                        <p class="fs-5"><?= $data->Tanggal; ?> | <?= $data->Jam; ?></p>
-                      </b>
-                    </div>
-                  </div>
-                  <div class="col-md-4 mb-2">
-                    <div class="bg-warning text-dark p-3 highlight">
-                      <h4 class="text-uppercase fw-light fs-3">Area</h4>
-                      <b>
-                        <p class="fs-5"><?= $data->Area; ?></p>
-                      </b>
-                    </div>
-                  </div>
-                  <div class="col-md-4 mb-2">
-                    <div class="bg-danger text-light p-3 highlight">
-                      <h4 class="text-uppercase fw-light fs-3">Magnitude</h4>
-                      <b>
-                        <p class="fs-5"><?= $data->Magnitude; ?></p>
-                      </b>
-                    </div>
-                  </div>
-                  <div class="col-md-12 mb-4">
-                    <div class="bg-info text-dark p-3 highlight">
-                      <h4 class="text-uppercase fw-light fs-3">Detail</h4>
-                      <div class="row p-1">
                         <div class="col-md-6">
                           <b>
-                            <p class="mb-0">Bujur: <?= $data->Bujur; ?></p>
-                            <p class="mb-0">Lintang: <?= $data->Lintang; ?></p>
-                            <p>Kedalaman: <?= $data->Kedalaman; ?></p>
+                            <p class="mb-0"><?= $data->Wilayah ?></p>
                           </b>
                         </div>
-                        <div class="col-md-6 d-flex justify-content-end align-self-end">
-                          <p><a href="<?= $data->Linkdetail ?>" class="fw-light text-dark fst-italic" target="_blank" rel="noopener noreferrer"><?= $data->Linkdetail ?></a></p>
+                        <div class="col-md-3">
+                          <img src="https://data.bmkg.go.id/DataMKG/TEWS/<?= $data->Shakemap ?>" width="200px" alt="Shakemap">              
                         </div>
                       </div>
                     </div>
@@ -163,6 +104,8 @@
                 </div>
 
                 <?php endforeach; ?>
+
+                <!-- Gempabumi  -->
 
                 <h5 class="card-title fs-3 mb-3">Data Gempabumi Di Indonesia</h5>
                 <div class="row">
@@ -175,12 +118,12 @@
                           <th>Posisi</th>
                           <th>Magnitude</th>
                           <th>Kedalaman</th>
-                          <th>Keterangan</th>
+                          <th>Wilayah</th>
                         </tr>
                       </thead>
                       <tbody>
                         <?php
-                          $gempa = "https://data.bmkg.go.id/gempadirasakan.xml";
+                          $gempa = "https://data.bmkg.go.id/DataMKG/TEWS/gempadirasakan.xml";
                           $temp = file_get_contents($gempa);
                           $xml = simplexml_load_string($temp);
                           $index = 1;
@@ -190,11 +133,11 @@
 
                         <tr>
                           <td><?= $index++ ?></td>
-                          <td><?= $data->Tanggal ?></td>
+                          <td><?= $data->Tanggal ?> - <?= $data->Jam ?></td>
                           <td><?= $data->Posisi ?></td>
                           <td><?= $data->Magnitude ?></td>
                           <td><?= $data->Kedalaman ?></td>
-                          <td><?= $data->Keterangan ?></td>
+                          <td><?= $data->Wilayah ?></td>
                         </tr>
 
                         <?php endforeach; ?>
@@ -211,8 +154,10 @@
       </div>
     </div>
 
-    <!-- Optional JavaScript; choose one of the two! -->
+    <!-- Jquery -->
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+    <!-- Datatables -->
     <script src="https://cdn.datatables.net/1.10.23/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.10.23/js/dataTables.bootstrap5.min.js"></script>
     <script>
